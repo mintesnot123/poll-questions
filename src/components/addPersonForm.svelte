c139e1e38494c1022270ab5985e93f4bbefc9528
mintestot123 <yismawmintesnot@gmail.com>
2022-11-05 02:38:15 -0700

    added modal element

---
+++ src/components/addPersonForm.svelte
@@ -0,0 +1,39 @@
+<script>
+    let firstName = "";
+    let lastName = "";
+    let age;
+    let skills = [];
+    let profesion;
+    const handleSubmit = () => {
+        console.log(
+            firstName,
+            " ",
+            lastName,
+            " ",
+            age,
+            " ",
+            skills,
+            " ",
+            profesion
+        );
+    };
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+    <p>Add New Person</p>
+    <input type="text" placeholder="First Name" bind:value={firstName} />
+    <input type="text" placeholder="Last Name" bind:value={lastName} />
+    <input type="number" placeholder="Age" bind:value={age} /><br />
+    <input type="checkbox" bind:group={skills} value="fighting" />fighting<br />
+    <input type="checkbox" bind:group={skills} value="sneaking" />sneaking<br />
+    <input type="checkbox" bind:group={skills} value="running" />running<br />
+    <select name="" id="" bind:value={profesion}>
+        <option value="front-end">Front-end developer</option>
+        <option value="back-end">Back-end developer</option>
+        <option value="full-stack">Full-stack developer</option>
+    </select>
+    <button type="submit">Add</button>
+</form>
+
+<style>
+</style>
---
+++ src/elements/Modal.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let isOpen = false;
+</script>
+
+{#if isOpen}
+    <div class="backdrop" on:click|self>
+        <div class="modal">
+            <slot />
+        </div>
+    </div>
+{/if}
+
+<style>
+    .backdrop {
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.5);
+        position: fixed;
+    }
+    .modal {
+        background-color: white;
+        margin: 10% auto;
+        max-width: 400px;
+        padding: 10px;
+        border-radius: 6px;
+        text-align: center;
+    }
+</style>
